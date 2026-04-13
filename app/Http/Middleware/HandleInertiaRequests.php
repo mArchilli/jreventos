4d639947cb9b911c65058e5ec4b2e94ddc74fa3e
Matias <archillimatias@gmail.com>
2026-04-13 02:06:49 -0300

    actualizacion footer

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use App\Models\Event;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 
@@ -34,6 +35,7 @@ class HandleInertiaRequests extends Middleware
             'auth' => [
                 'user' => $request->user(),
             ],
+            'events' => Event::select('id', 'title')->orderBy('title')->get(),
         ];
     }
 }
